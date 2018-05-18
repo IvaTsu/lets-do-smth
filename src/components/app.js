f77ce5ac4cbe4a0afdb36cab58280471ffcbc6b6
IvaTsu <ivan.tsud@gmail.com>
2018-05-18 17:44:22 +0300

    added tasks array to create a list of tasks

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,14 @@
 import React from 'react';
 
+const tasks = [
+    {
+        task: 'read book',
+        isCompleted: false
+    },
+    {
+        task: 'do exercises',
+        isCompleted: false
+    }
+];
+
 export default () => <h1>Hello!</h1>;
