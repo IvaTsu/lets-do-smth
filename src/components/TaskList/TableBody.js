68b5c5465eadb6a8923c087842ac339750a7a28f
IvaTsu <ivan.tsud@gmail.com>
2018-05-19 19:32:40 +0300

    added isEditing state to render different set of needed managing btns

--- src/components/TaskList/TableBody.js
+++ src/components/TaskList/TableBody.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 export default class TableBody extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        }
+    }
+
+    _onClick = () => {
+        this.setState({
+            isEditing: !this.state.isEditing
+        })
+    }       
+
     render() {
+        const { item } = this.props;
+        const renderBtnsOnActiveEdit = this.state.isEditing ? (
+                <td>
+                    <button onClick={this._onClick}>Save</button>
+                    <button>Cancel</button>
+                </td>
+        ) : (
+            <td>
+                <button onClick={this._onClick}>Edit</button>
+                <button>Exit</button>
+            </td>
+        );
         return (
             <tr>
                 <td>{item}</td>
-                <button>Edit</button>
-                <button>Delete</button>
+                {renderBtnsOnActiveEdit}
             </tr>
         );
     }
