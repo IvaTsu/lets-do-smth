7137e221c723ff0b7ccd24e423396415a04cebce
IvaTsu <ivan.tsud@gmail.com>
2018-05-20 15:32:27 +0300

    merged config-setup brunch into feature

--- src/components/app.js
+++ src/components/app.js
@@ -3,34 +3,31 @@ import TaskList from './TaskList/TaskList';
 import CreateTask from './CreateTask';
 
 const tasks = [
-    {
-        task: 'read book',
-        isCompleted: false
-    },
-    {
-        task: 'do exercises',
-        isCompleted: false
-    },
-    {
-        task: 'do eercises',
-        isCompleted: false
-    }
+  {
+    task: 'read book',
+    isCompleted: false,
+  },
+  {
+    task: 'do exercises',
+    isCompleted: false,
+  },
+  {
+    task: 'do eercises',
+    isCompleted: false,
+  },
 ];
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks
-        };
-    }
-    render() {
-        return(
-            <div>
-                <CreateTask />
-                <TaskList tasks={tasks} />
-            </div>
-            
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <CreateTask />
+        <TaskList tasks={tasks} />
+      </div>
+
+    );
+  }
 }
