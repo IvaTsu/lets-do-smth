efa6831f1092a7313d5ec3a0a94aad9d86757d54
IvaTsu <ivan.tsud@gmail.com>
2018-05-21 21:14:44 +0300

    add PropTypes

--- src/components/TaskList/TableBody.js
+++ src/components/TaskList/TableBody.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 
 export default class TableBody extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEditing: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+    };
+  }
 
-    _onEditClick = () => {
-        this.setState({
-            isEditing: true
-        });
-    }
+  _onEditClick = () => {
+    this.setState({
+      isEditing: true,
+    });
+  }
 
-    _onCancelClick = () => {
-        this.setState({
-            isEditing: !this.state.isEditing
-        });
-    }
+  _onCancelClick = () => {
+    this.setState({
+      isEditing: !this.state.isEditing,
+    });
+  }
 
-    render() {
-        const { item } = this.props;
-        const renderBtnsOnActiveEdit = this.state.isEditing ? (
-                <td>
-                    <button>Save</button>
-                    <button onClick={this._onCancelClick}>Cancel</button>
-                </td>
-        ) : (
-            <td>
-                <button onClick={this._onEditClick}>Edit</button>
-                <button>Exit</button>
-            </td>
-        );
-        return (
-            <tr>
-                <td>{item}</td>
-                {renderBtnsOnActiveEdit}
-            </tr>
-        );
-    }
+  render() {
+    const { item } = this.props;
+    const renderBtnsOnActiveEdit = this.state.isEditing ? (
+      <td>
+        <button>Save</button>
+        <button onClick={this._onCancelClick}>Cancel</button>
+      </td>
+    ) : (
+      <td>
+        <button onClick={this._onEditClick}>Edit</button>
+        <button>Exit</button>
+      </td>
+    );
+    return (
+      <tr>
+        <td>{item}</td>
+        {renderBtnsOnActiveEdit}
+      </tr>
+    );
+  }
 }
