c3989336ceca48b7a7d98790db533d49b77155d8
IvaTsu <ivan.tsud@gmail.com>
2018-05-22 17:56:29 +0300

    edit config with babel and hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
           presets: [
